c9f0a72ffe2367be0e6d488a6a165547fa794a6b
salehalenezi20 <142608912+salehalenezi20@users.noreply.github.com>
2023-09-11 19:23:08 +0300

    updates

--- final_project.py
+++ final_project.py
@@ -7,46 +7,46 @@ import ttkbootstrap
 # the api from open weather
 
 def get_weather(city):
-    API_key="0d9a41652b4e798eb120e24cca445fad"
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}"
-    res = requests.get(url)
+    API_k="0d9a41652b4e798eb120e24cca445fad"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_k}"
+    resp = requests.get(url)
 
 # error
 
-    if res.status_code == 404:
+    if resp.status_code == 404:
         messagebox.showerror("Error", "city not found")
         return None
     
 #the temp and city 
      
-    weather = res.json()
+    weather = resp.json()
     icon_id = weather['weather'][0]['icon']
-    temperature = weather['main']['temp'] - 273.15
-    description = weather['weather'][0]['description']
+    temp = weather['main']['temp'] - 273.15
+    des = weather['weather'][0]['description']
     city = weather['name']
     country = weather['sys']['country']
 
     icon_url = f"https://openweathermap.org/img/wn/{icon_id}@2x.png"
-    return (icon_url, temperature, description, city, country)
+    return (icon_url, temp, des, city, country)
 
 
 #the temp results 
 
 def search():
-    city = city_entry.get()
+    city = city_ent.get()
     result = get_weather(city)
     if result is None:
         return
-    icon_url,  temperature, description, city, country = result
-    loacation_label.configure(text=f"{city}, {country}")
+    icon_url,  temp, des, city, country = result
+    loacation_1.configure(text=f"{city}, {country}")
 
     image = Image.open(requests.get(icon_url, stream=True).raw)
     icon = ImageTk.PhotoImage(image)
-    icon_label.configure(image=icon)
-    icon_label.image = icon 
+    icon_lab.configure(image=icon)
+    icon_lab.image = icon 
 
-    temperature_label.configure(text=f"Temperature: {temperature:.2f}°C")
-    description_label.configure(text=f"Description: {description}")
+    temperature_lab.configure(text=f"Temperature: {temp:.2f}°C")
+    description_1.configure(text=f"Description: {des}")
 
 
 # geometry and title
@@ -56,23 +56,23 @@ root.title("weather app")
 root.geometry("400x400") 
 
 
-city_entry = ttkbootstrap.Entry(root, font="Helvetica, 18")
-city_entry.pack(pady=10)
+city_ent = ttkbootstrap.Entry(root, font="Helvetica, 18")
+city_ent.pack(pady=10)
 
 
-search_button= ttkbootstrap.Button(root, text="Search", command=search, bootstyle="warning")
-search_button.pack(pady=10)
+search = ttkbootstrap.Button(root, text="Search", command=search, bootstyle="warning")
+search.pack(pady=10)
 
-loacation_label = tk.Label(root, font="Helvetica, 25")
-loacation_label.pack(pady=20)
+loacation_1 = tk.Label(root, font="Helvetica, 25")
+loacation_1.pack(pady=20)
 
-icon_label = tk.Label(root)
-icon_label.pack()
+icon_lab = tk.Label(root)
+icon_lab.pack()
 
-temperature_label = tk.Label(root, font="Helvetica, 20")
-temperature_label.pack()
+temperature_lab = tk.Label(root, font="Helvetica, 20")
+temperature_lab.pack()
 
-description_label = tk.Label(root, font="Helvetica, 20")
-description_label.pack()
+description_1 = tk.Label(root, font="Helvetica, 20")
+description_1.pack()
 
 root.mainloop()
